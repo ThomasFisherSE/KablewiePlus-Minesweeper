075983c58b237d69c7922c92b405f9a8f91a6493
Ethan Davies <Ethan@ethans-mbp.home>
2016-03-05 15:27:12 +0000

    Tried, I repeat...tried, some probabilty stuff with the code, currently commented out

--- src/game/Computer.java
+++ src/game/Computer.java
@@ -14,13 +14,35 @@ public class Computer extends Player implements Runnable {
 	private boolean m_aiToggled = false;
 	private Board m_board;
 	private GameController m_gameController;
-	private final int SLEEP_TIME = 500;
+	private final int SLEEP_TIME = 2000;
+	private boolean easyFlag;
+	private boolean normalFlag;
+	private boolean perfectFlag;
+
 	
 	// AI Difficulty Probabilities
 	private final double PERFECT_PROBABILITY = 1; // i.e. will always play perfectly
 	private final double NORMAL_PROBABILITY = 0.9; // i.e. 1/10 moves won't be perfect
-	private final double EASY_PROBABILITY = 0.5; // i.e. only 50% chance to make a perfect move
+	private final double EASY_PROBABILITY = 50.0; // i.e. only 50% chance to make a perfect move
+	private final double MAXIMUM = 100;
+	private final double MINIMUM = 1;
 	
+	private void easyDifficulty() {
+		double randomDouble = new Random().nextInt((int)MAXIMUM - (int)MINIMUM + 1);
+		if (randomDouble <= (EASY_PROBABILITY)) {
+			easyFlag = true;
+			System.out.println("Prob val: " + EASY_PROBABILITY);
+			System.out.println("Easy flag: " + easyFlag);
+			System.out.println(randomDouble);
+			System.out.println("Less than probability");
+		} else if (randomDouble > (EASY_PROBABILITY)) {
+			easyFlag = false;
+			System.out.println("Prob val: " + EASY_PROBABILITY*2);
+			System.out.println("Easy flag: " + easyFlag);
+			System.out.println(randomDouble);
+			System.out.println("More than probability");
+		}
+	}
 	
 	public boolean toggleAi() {
 		m_aiToggled = !m_aiToggled;
@@ -60,6 +82,7 @@ public class Computer extends Player implements Runnable {
 	}
 	
 	private boolean makeMove() {
+		//easyDifficulty();
 		Random rnd = new Random();
 		int row = rnd.nextInt(m_board.getm_Board().size());
 		int column = rnd.nextInt(m_board.getm_Board().get(row).size());
@@ -70,10 +93,18 @@ public class Computer extends Player implements Runnable {
 			System.out.println("Revealed tile: (" + row + "," + column + ")");
 			return true;
 		} else if (randomTile.isMine() && !randomTile.isDefused()) {
-			m_board.defusedTile(column, row);
-			m_gameController.repaintAll();
-			System.out.println("Mine  tile found. Looping.");
-			return true;
+			/* if (easyFlag = true) {
+				m_board.revealTile(column,row);
+				m_gameController.repaintAll();
+				System.out.println("Mine tile exploded.");
+				easyFlag = false;
+				return false;
+			} else {*/
+				m_board.defusedTile(column, row);
+				m_gameController.repaintAll();
+				System.out.println("Mine tile found. Looping.");
+				return true;
+			//}
 		} else if (randomTile.isMine() && randomTile.isDefused()) {
 			System.out.println("Mine already defused. Looping.");
 			return false;
