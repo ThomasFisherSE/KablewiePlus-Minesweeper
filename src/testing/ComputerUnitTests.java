5b12de980f21fdae302fb59533870b0e1e761f98
Ethan Davies <Ethan@Ethans-MacBook-Pro.local>
2016-03-11 13:00:41 +0000

    Added some unit tests for computer

---
+++ src/testing/ComputerUnitTests.java
@@ -0,0 +1,14 @@
+package testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ComputerUnitTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
