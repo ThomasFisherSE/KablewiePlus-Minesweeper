41eb957356c24d9632ecd4920ab90752db805d62
Vicky Charvis <vicchar@live.co.uk>
2016-03-12 21:01:02 +0000

    Update Human.java
    meets doxygen rules

--- src/game/Human.java
+++ src/game/Human.java
@@ -2,27 +2,41 @@
  * @file Human.java
  * @author Zongbo Xu
  * @date 5 December 2015
+ * @see Player.java
+ * @brief A class for Human players.
  *
- * A class for Human players.
+ * A class for Human players, inherits from Player
  */ 
 
 package game;
 
 public class Human extends Player  {
-	private boolean m_playersTurn = true;
-
+	
+	/**
+	 * Gets if it's the human players turn
+	 * 
+	 * @return boolean if it's the human players turn
+	 */
+	public boolean getPlayersTurn() {
+		return m_playersTurn;
+	}
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param name the players name
+	 */
 	public Human(String name) {
 		super(name);
 	}
 	
+	/**
+	 * Sets if it's the human player turn
+	 */
 	public void takeTurn() {
 		m_playersTurn = true;
 	}
 
-	public boolean getPlayersTurn() {
-		return m_playersTurn;
-	}
-
-	
+	private boolean m_playersTurn = true;
 
 }
