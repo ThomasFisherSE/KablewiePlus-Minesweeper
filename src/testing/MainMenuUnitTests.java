7c4f37594dffcd94bb60bf01f644902474facd7f
erogideon <gidbrisssy@live.co.uk>
2016-03-13 16:33:56 +0000

    Update MainMenuUnitTests.java

--- src/testing/MainMenuUnitTests.java
+++ src/testing/MainMenuUnitTests.java
@@ -1,6 +1,17 @@
+/**
+ * @file MainMenuUnitTests.java
+ * @author Gideon Eromosele
+ * @Date 12/03/2016
+ * @see MainMenu.java
+ * @brief Tests MainMenu class
+ * 
+ * Tests all method in MainMenu class with valid and invalid input
+ */
 package testing;
 
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert.*;
 
 import main.*;
 
@@ -8,20 +19,28 @@ import org.junit.Test;
 
 public class MainMenuUnitTests {
 	IntegrationTests interactingClass = new IntegrationTests();
-
+	
+	/** 
+	 * Testing to the testStartGame with valid input 
+	 */
 	@Test
 	public void startGameTest1() {
 		MainMenu test = new MainMenu(interactingClass.createFrame(), new Kablewie());
 		
-		assertEquals("Test if the inputs are valid to start Game", true, 
+		//The statement is set to false, if and only if inputs to start game is true
+		assertEquals("Test if the inputs are valid to start Game", false, 
 				test.startGame());
 	}
-
+	
+	/** 
+	 * Testing to the testStartGame with valid input 
+	 */
 	@Test
 	public void startGameTest2() {
 		MainMenu test = new MainMenu(interactingClass.createFrame(), new Kablewie());
 		
+		//The statement is set to true, if and only if inputs to start game is false
 		assertEquals("Test if the inputs are not valid to start Game",
-				false, test.startGame());
+				true, test.startGame());
 	}
 }
